22b26921edc72d3a18d9a05eb5306032af1d99c9
yaseer <yaseer@pickme.lk>
2017-11-10 12:29:12 +0530

    friday morning

--- LearningPlattform/Migrations/Configuration.cs
+++ LearningPlattform/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace LearningPlattform.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(LearningPlattform.Models.ApplicationDbContext context)
